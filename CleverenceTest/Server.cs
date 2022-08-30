91e6d19527cbcadb006a9b78b6a04dbdc3978805
Andrey <morgunof-andrey@yandex.ru>
2022-08-30 11:23:27 +0300

    Add Server class

---
+++ CleverenceTest/Server.cs
@@ -0,0 +1,36 @@
+﻿namespace CleverenceTest;
+
+public static class Server
+{
+    private static int count;
+    private static readonly ReaderWriterLockSlim readerWriterLock = new ReaderWriterLockSlim();
+
+    public static void AddToCount(int value)
+    {
+        readerWriterLock.EnterWriteLock();
+        try
+        {
+            checked
+            {
+                count += value;
+            }
+        }
+        finally
+        {
+            readerWriterLock.ExitWriteLock();
+        }
+    }
+
+    public static int GetCount()
+    {
+        readerWriterLock.EnterReadLock();
+        try
+        {
+            return count;
+        }
+        finally
+        {
+            readerWriterLock.ExitReadLock();
+        }
+    }
+}
